08032dd708cd5590a440ba703356bc0a3dcfeb6a
syn8228 <87139967+syn8228@users.noreply.github.com>
2021-08-22 16:49:26 +0800

    Update Siamese.py

--- baselines/Siamese.py
+++ baselines/Siamese.py
@@ -311,10 +311,19 @@ class TrainList(Dataset):
         label = random.randint(0, 1)
         if label == 0:
             query_image = Image.open(self.image_list[i])
+            query_image = query_image.convert("RGB")
             db_image = self.argumentation(query_image)
+            if self.transform is not None:
+                query_image = self.transform(query_image)
+                db_image = self.transform(db_image)
         else:
             query_image = Image.open(self.image_list[i])
             db_image = Image.open(random.sample(self.image_list, 1)[0])
+            query_image = query_image.convert("RGB")
+            db_image = db_image.convert("RGB")
+            if self.transform is not None:
+                query_image = self.transform(query_image)
+                db_image = self.transform(db_image)
         return query_image, db_image, label
 
 
